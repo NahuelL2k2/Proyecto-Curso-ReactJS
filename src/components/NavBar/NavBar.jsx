c647be1b829083fbe075abe44953b8cb378ecde6
Nahuel Leiva <nahuelleiva2002@gmail.com>
2023-04-29 22:03:49 -0300

    pequeños cambios al header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,11 +2,13 @@ import "./NavBar.css"
 import SearchBar from "../SearchBar/SearchBar"
 
 function NavBar() {
+  // const logoHeaderPrueba = '../../assets/logoHeader.png'
+  const logoHeader = "https://static.vecteezy.com/system/resources/thumbnails/008/976/210/small/clothing-store-logo-design-with-hanger-illustration-vector.jpg"
   return (
     <header>
       <div className="subNav1">
-        <h1>Logo de la tienda</h1>
-
+        <img className="logoHeader" src={logoHeader} alt=""/>
+        {/* <img src={require('../../assets/logoHeader.png')} alt="" /> */}
         <SearchBar />
 
         <ul>
